47b8165a4aeeb3b5d1bba9fb316bfd3a49694859
Anderson Grüdtner Martins <andergmartins@gmail.com>
2014-06-23 16:47:39 -0300

    Add initial code

---
+++ src/wistiaembed.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   plg_content_wistiaembed
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2013 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * Wistia Video Embedder Content Plugin
+ *
+ */
+class plgContentWistiaEmbed extends JPlugin
+{
+    public function __construct(&$subject, $config = array())
+    {
+        parent::__construct($subject, $config);
+
+        $lang = JFactory::getLanguage();
+        $lang->load('plg_content_wistiaembed.sys', __DIR__);
+    }
+
+    /**
+     * @param string $context
+     * @param object $article
+     * @param object $params
+     * @param int    $page
+     *
+     * @return bool
+     */
+    public function onContentPrepare($context, &$article, &$params, $page = 0)
+    {
+        if (JString::strpos($article->text, '{wistia') === false) {
+            return true;
+        }
+
+
+
+        return true;
+    }
+
+    protected function wistiaCodeEmbed($vCode)
+    {
+        $output = '';
+
+        return $output;
+    }
+}
