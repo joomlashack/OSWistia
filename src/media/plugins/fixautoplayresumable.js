c4cb2c43010803579e14f58bfb947ea41f02ee4a
Anderson Grüdtner Martins <andergmartins@gmail.com>
2014-06-24 16:05:25 -0300

    Fix the date on the file header

--- src/media/plugins/fixautoplayresumable.js
+++ src/media/plugins/fixautoplayresumable.js
@@ -1,7 +1,7 @@
 /**
  * @package   com_oswistia
  * @contact   www.ostraining.com, support@ostraining.com
- * @copyright 2013 Open Source Training, LLC. All rights reserved
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 /* global Wistia */
