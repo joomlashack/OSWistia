8570c0939d2400f8b55d643a07bf272e9ae28660
Anderson Grudtner Martins <anderson@ostraining.com>
2017-10-18 10:57:30 -0200

    Add persistency to the selected video speed, WIST-45

--- src/media/js/oswistia.js
+++ src/media/js/oswistia.js
@@ -7,6 +7,10 @@
 
 (function($, window) {
 	window.OSWistiaInit = function(video) {
+		function getButtonForSettingOnPlayer(setting, value) {
+			return $('.w-menu__list-item--' + setting + ' li button[data-optionkey="' + value + '"]');
+		}
+
 		// Timeout to wait until the button is there
 		window.setTimeout(function() {
 			// Add persistence do the video quality if set by the user.
@@ -19,15 +23,39 @@
 					quality = $button.data('optionkey');
 				
 				// Register the new value
-				window.Wistia.localStorage('oswistia_quality', quality);
+				window.Wistia.localStorage('quality', quality);
 			});
 
 			// Make sure we are using the quality selected by the user
-			var quality = window.Wistia.localStorage('oswistia_quality'),
-				$button = $('.w-menu__list-item--Quality li button[data-optionkey="' + quality + '"]');
+			var quality = window.Wistia.localStorage('quality'),
+				$button = getButtonForSettingOnPlayer('Quality', quality);
 
 			$button.trigger('click');
 
-		}, 1000);
+
+			// Add persistence do the video speed if set by the user.
+			// Neighter Wistia or HTML5 don't provide an API to intercept
+			// the change of the video speed. So we need to intercept the 
+			// click events.
+			$('.w-menu__list-item--Speed li button').click(function() {
+				// Get the new speed
+				var $button = $(this),
+					speed = $button.data('optionkey');
+				
+				// Register the new value
+				window.Wistia.localStorage('playbackRate', parseFloat(speed));
+			});
+
+			// Make sure we are using the speed selected by the user
+			var speed = window.Wistia.localStorage('playbackRate'),
+				$button = getButtonForSettingOnPlayer('Speed', speed);
+			
+			$button.trigger('click');
+			
+			// Use a timeout to make sure we override any default setting
+			window.setTimeout(function() {
+				video.playbackRate(parseFloat(speed));
+			}, 500);
+		}, 500);
 	};
 })(jQuery, window);
